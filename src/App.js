fdfa1654015842c19a97296f1e07fff98914b78b
Dallas James <dallasjames42@gmail.com>
2019-08-28 18:16:39 -0600

    got it all styled in react except the body and code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,51 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
-
+import styled from 'styled-components' 
 import "./App.css";
 
 function App() {
+
+  const HeaderDiv = styled.div`
+    display: flex;
+    justify-content: center;
+
+    `;
+  const LogoDiv = styled.div`
+    > img {
+      width: 10rem;
+    }
+  `;
+
+  const ContentDiv = styled.div`
+    display: flex;
+    justify-content: center;
+  `;
+
+  const PicDiv = styled.div`
+    margin-left: 2rem;
+    > img{
+      width: 100%;
+    }  
+
+    > p {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+  `;
+
+  const AboutDiv = styled.div`
+    margin-right: 2rem;
+    margin-left: 2rem;
+  `; 
+
   const [nasaPic, setNasaPic] = useState(""); 
   const [date, setDate] = useState("");
   const [explain, setExplain] = useState("");
 
   useEffect(() => {
     axios
-      .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+      .get("https://api.nasa.gov/planetary/apod?api_key=ZyvT3UKrzUTVWizhQ9VK6CJjdG2gaGT9bTWExerM")
       .then((response) => {
         console.log(response);
         setNasaPic(response.data.hdurl);
@@ -22,24 +57,24 @@ function App() {
   }, [setNasaPic, nasaPic, setDate, date, setExplain, explain]);
 
   return (
-    <div className="App">
-      <div className='header'>
-        <div className='logo'>
+    <div>
+      <HeaderDiv>
+        <LogoDiv>
           <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/1200px-NASA_logo.svg.png' alt='nasa logo'></img>
-        </div>
-        <div className="headText">
+        </LogoDiv>
+        <div>
           <h1>PHOTO OF THE DAY</h1>
         </div>
-      </div>
-      <div className="content">
-        <div className="pic">
+      </HeaderDiv>
+      <ContentDiv>
+        <PicDiv>
           <img src={nasaPic} alt="pic of the day"></img>
           <p>{date}</p>
-        </div>
-        <div className="about">
+        </PicDiv>
+        <AboutDiv>
           <p>{explain}</p>
-        </div>
-      </div>
+        </AboutDiv>
+      </ContentDiv>
     </div>
   );
 }
